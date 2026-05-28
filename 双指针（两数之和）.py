126fb96fce2d75cb2f820d465187779b6d09a70d
Mengyu Zhang <51690044+z527201599@users.noreply.github.com>
2026-05-28 10:28:41 +0800

    Add comments for two-pointer solution explanation
    Add comments explaining the two-pointer approach for solving 'Two Sum II'.

--- 双指针（两数之和）.py
+++ 双指针（两数之和）.py
@@ -1,3 +1,6 @@
+# 解决“两数之和 II - 输入有序数组”，核心思想是利用数组的有序性，通过两个指针从两端向中间逼近。
+# 达到时间复杂度O（n）【只遍历一遍】，空间复杂度O（1）【常数级的额外变量】
+
 def twoSum_twoPointer(nums, target):
     left, right = 0, len(nums) - 1
     while left < right:
