e0f778e2414fc36b768ac9196a7d76379d9ca22a
Mengyu Zhang <51690044+z527201599@users.noreply.github.com>
2026-06-25 09:57:44 +0800

    Add MinStack class with push, pop, and getMin methods
    Implement MinStack with auxiliary stack to track minimum values.

---
+++ 栈（辅助栈 解最小栈）.py
@@ -0,0 +1,26 @@
+# 要在 o1 的时间复杂度弹出最小栈，不能用遍历法，核心思路是 用第二个栈 辅助栈：
+# 主栈正常存数据，辅助栈 同步记录每一步的最小值
+# push时候 辅助栈压入 当前值和栈顶的较小者。getMin时 直接读辅助栈的栈顶。
+# 辅助栈 的每一层，都记住了 当时 的最小值。
+
+class MinStack:
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        cur_min = val
+        if self.min_stack:
+            cur_min = min(val, self.min_stack[-1])
+        self.min_stack.append(cur_min)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
