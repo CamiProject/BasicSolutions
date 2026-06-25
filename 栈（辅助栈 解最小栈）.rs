6ce17d81a9bc3749eff78d4298e210a493121c99
Mengyu Zhang <51690044+z527201599@users.noreply.github.com>
2026-06-25 11:50:32 +0800

    Implement MinStack with push, pop, top, and get_min

--- 栈（辅助栈 解最小栈）.rs
+++ 栈（辅助栈 解最小栈）.rs
@@ -5,3 +5,34 @@ struct MinStack {
     min_stack: Vec<i32>,
 }
 
+impl MinStack {
+    fn new() -> Self {
+        MinStack {
+            stack: Vec::new(),
+            min_stack: Vec::new(),
+        }
+    }
+    
+    fn push(&mut self, val: i32) {
+        self.stack.push(val);
+        
+        //使用 match 或 if let 处理空栈
+        match self.min_stack.last() {
+            Some(&min) => self.min_stack.push(val.min(min)),
+            None => self.min_stack.push(val),
+        }
+    }
+    
+    fn pop(&mut self) {
+        self.stack.pop();
+        self.min_stack.pop();
+    }
+    
+    fn top(&self) -> i32 {
+        self.stack.last().copied().unwrap()
+    }
+
+    fn get_min(&self) -> i32 {
+        self.min_stack.last().copied().unwrap()
+    }
+}
