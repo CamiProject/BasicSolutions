75b11b502b3e28f79473776df46a9e512fe65f44
Mengyu Zhang <51690044+CamiProject@users.noreply.github.com>
2026-08-10 15:59:40 +0800

    Update python_api_app_or_router.py

--- 个人/python_api_app_or_router.py
+++ 个人/python_api_app_or_router.py
@@ -2,17 +2,22 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+import sqlite3
 
-app = FastAPI
+DB_PATH = "./backend/user.db"
+
+app = FastAPI()
 
 class User(BaseModel):
     id: int
     name: str
     description: str
 
-@app.get('/user/user_id')
-async def get_user_name(user_id) -> User:
-    user_name = await db.get()
+@app.get('/user/{user_id}')
+async def get_user_name(user_id: int) -> User:
+    async with db_pool.connect as conn:
+        row = await conn.fetch("SELECT id FROM WHERE GROUPBY")
+    user_name = row.name
     return User(id = user_id, name = user_name, description = "developer")
 
 # router写法是：
