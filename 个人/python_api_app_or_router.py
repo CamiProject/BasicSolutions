e7f881876cfc6d2a1ab815a73e9bac98b8264325
Mengyu Zhang <51690044+CamiProject@users.noreply.github.com>
2026-08-10 15:22:13 +0800

    Update python_api_app_or_router.py

--- 个人/python_api_app_or_router.py
+++ 个人/python_api_app_or_router.py
@@ -1,4 +1,4 @@
-# 有没有prefix的区别主要是结构化、和规整（swagger）
+# 有没有prefix的区别主要是结构化、和规整（swagger），app写法是：
 
 from fastapi import FastAPI
 from pydantic import BaseModel
@@ -6,9 +6,16 @@ from pydantic import BaseModel
 app = FastAPI
 
 class User(BaseModel):
-  id: int
-  name: str
-  description: str
+    id: int
+    name: str
+    description: str
 
 @app.get('/user/user_id')
 async def get_user_name(user_id) -> User:
+    user_name = await db.get()
+    return User(id = user_id, name = user_name, description = "developer")
+
+# router写法是：
+
+from fastapi import FastAPI
+from pydantic import 
